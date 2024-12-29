9b4af63cfe534db215e261ce4c100f4206422d40
Tejas Ghag <tejas.ghag@somaiya.edu>
2024-12-29 11:45:52 +0000

    conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,20 @@
+let age= "18aa" //MAke sure there is no alphabets in the vaiable while changing the data type
+console.log(typeof age)
+
+let convert= Number(age) // vari name= Data type(source variable)
+//First letter of data type should be capital
+console.log(typeof convert)
+console.log(convert)
+
+/*
+null= 0
+undefined,string= NaN
+true/false= 1/0
+*/
+
+let log="qwerty"
+console.log(log)     
+let boo=Boolean(log)
+console.log(boo)
+// 1,"..",..=>True
+//0," "=>false
