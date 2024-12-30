009a369b9fe4f95ab493005a8e3e480a93e50747
Tejas Ghag <tejas.ghag@somaiya.edu>
2024-12-30 16:07:19 +0000

    opeerands

---
+++ Basics/operands.js
@@ -0,0 +1,26 @@
+let no =10
+let neg=-(no)
+//console.log(neg);
+
+st1= "Tejas"
+st2= "Codes"
+//console.log(st1 + st2);
+
+// console.log(3+4);     //7
+// console.log(2+"3"+4); //234
+// console.log(2+3+"4"); //54
+
+//Correct fromat to write eqn
+// console.log((3+4)*100%2);
+
+count=1
+console.log(count);
+++count //prefix increment
+console.log(count);
+count+=4 //post increment
+console.log(count);
+
+
+
+
+
