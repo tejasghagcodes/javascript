030059c340c138fb47d58f32f157472afb98978a
Tejas Ghag <tejas.ghag@somaiya.edu>
2024-12-30 17:09:33 +0000

    comparison

---
+++ Basics/compare.js
@@ -0,0 +1,11 @@
+// console.log(2==2);
+// console.log(1<=5);
+// console.log(2>=1);
+// console.log(2!=1);
+// console.log("1">2);
+
+console.log(null!=0); //null is >= or <= 0
+console.log(undefined ==0)// undefined gives false value to ebry comparison
+
+console.log("3" === 3)//strict check (checks "datatypes" and value)
+ 
