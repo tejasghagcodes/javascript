3801b055e999d9197e6e880c8c2daeeba1bafd12
Tejas Ghag <tejas.ghag@somaiya.edu>
2024-12-31 22:02:42 +0000

    Dates

---
+++ Basics/date.js
@@ -0,0 +1,32 @@
+//Date is an object
+
+const day = new Date()
+// console.log(day.toJSON());
+// console.log(day.toLocaleDateString());
+// console.log(day.toTimeString());
+
+//let day2=  new Date(2025, 0, 23 ) //date(yy,mm,dd,hr,min,sec,)
+let day2=  new Date("1-28-2025")  //mm/dd/yyyy
+//let day2=  new Date("2025-1-10")   //yyyy/mm/dd
+//console.log(day2.toLocaleDateString());
+
+let Ts= Date.now()
+// console.log(Ts);
+// console.log(day2.getTime());
+
+//Convert to millisecond
+//console.log(Math.floor(Date.now()/1000));
+
+let day3 =new Date()
+// console.log(day3.getMonth()+1); //+1 to get to normal index 
+// console.log(day3.getDay(+1));// Same goes for day
+
+day3.toLocaleString('default',{
+    weekday:"short",
+    month:"long"
+})
+
+
+
+
+
--- Projects/Dice.js
+++ Projects/Dice.js
@@ -3,7 +3,4 @@ const max=6
 
 console.log(Math.floor(Math.random()*(max-min +1))+ min);
 
-
-
-//Learnings from 
-window.location.href = "vscode-remote://codespaces%2Bstunning-succotash-69xw57p6xw72pw/workspaces/javascript/Basics/num.js";
+// Learnings Useed from Basics/num.js
