e67c5a64d55602fb283ee46329efccbfe44023a3
Tejas Ghag <tejas.ghag@somaiya.edu>
2024-12-31 08:23:53 +0000

    String functions

---
+++ Basics/string.js
@@ -0,0 +1,29 @@
+const a="Superman";
+const b= 100 ;
+//console.log(`${a} flyes with speed of ${b} m/s`);
+
+const user= new String("qwertyuiop")  //Other way to declare string
+// console.log(user.length);
+// console.log(user[4]);
+// console.log(user.italics());
+// console.log(user.toUpperCase());
+// console.log(user.charAt(7));
+// console.log(user.indexOf('y'));
+
+let sub=a.substring(0,5)
+//console.log(sub);
+
+let slic=a.slice(-3,8) //-no. is final range //- no start from right i.e "S" is -7...
+//console.log(slic);
+
+let name="  hello t "
+//console.log(name.trim() +"ejas");
+
+let str ="Orange is my favourite color"
+// console.log(str.replace('color', "fruit"));
+// console.log(str.includes('my')); //Checks whether the given characters are present in the string
+
+let mail="tejas.ghag@qwerty.com"
+console.log(mail.split("@",[1])); //split("separator",[elements of array])
+
+
