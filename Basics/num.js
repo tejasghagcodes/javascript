225ebe8753cc501852f9fcbbccc094a01bf4ec3e
Tejas Ghag <tejas.ghag@somaiya.edu>
2024-12-31 21:13:34 +0000

    number and math

---
+++ Basics/num.js
@@ -0,0 +1,34 @@
+const marks= 100
+//console.log(marks);
+const bal= new Number(100000)
+// console.log(bal);
+
+// console.log(bal.toString().length); //when converted to string we can use string function
+// console.log(bal.toFixed(2)) //toFixed(no. of digit after decimal)
+num= 123.787
+//console.log(num.toPrecision(5)) //toPrecision(Total number of digits to be displayed) //op return as string
+//console.log(bal.toLocaleString()); //puts , to make number readable
+
+////////////////////   MATHS    ////////////////////
+
+
+// console.log(Math);
+// console.log(Math.abs(-7)); //absolute
+// console.log(Math.round(2.35)); //round to max=> .ceil()  //round to Min=> .floor()
+let score=[1,2,5,6,8,9,8,5,0,1,2,3,]
+//console.log(Math.max(...score)); // To access predefined array use ...arrayname 
+//console.log(Math.random()); //gives values from 0 to 1
+
+const min=1
+const max=6
+
+//console.log(Math.floor(Math.random()*(max-min +1))+ min);
+
+
+
+
+
+
+
+
+
