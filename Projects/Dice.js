ef1526376182b583cccc51e8e9cdf9bb2ed3aa13
Tejas Ghag <tejas.ghag@somaiya.edu>
2024-12-31 21:16:55 +0000

    Dice Game

---
+++ Projects/Dice.js
@@ -0,0 +1,9 @@
+const min=1
+const max=6
+
+console.log(Math.floor(Math.random()*(max-min +1))+ min);
+
+
+
+//Learnings from 
+window.location.href = "vscode-remote://codespaces%2Bstunning-succotash-69xw57p6xw72pw/workspaces/javascript/Basics/num.js";
